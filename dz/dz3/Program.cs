e49af2158ed401297fd069ce3e9cc8a2fe749cc2
Ekaterina6867 <katya.judo@mail.ru>
2022-12-06 15:26:31 +0300

    решили первую задачу

---
+++ dz/dz3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+void Pol(int number)
+{
+    if(number >= 10000)
+    {
+        int n = number / 10000;
+        int x = number % 10;
+ 
+            if(n == x)
+            {
+                number = number / 10;
+                int n1 = (number / 100) % 10;;
+                int x1 = number % 10;
+                if(n1 == x1)
+                    Console.WriteLine("Да");
+            }
+            else 
+            Console.WriteLine("Нет");
+            
+    }
+    else
+    Console.WriteLine("Некорректное число");
+}
+ 
+Console.WriteLine("Введите пятизначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+Pol(number);
