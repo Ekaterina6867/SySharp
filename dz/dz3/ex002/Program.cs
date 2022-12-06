d73628b640f5404835aceb7635a1f61c290b98c2
Ekaterina6867 <katya.judo@mail.ru>
2022-12-06 17:54:32 +0300

    решили вторую задачу

---
+++ dz/dz3/ex002/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n>0)
+{
+    int result=n*n*n;
+    n = (n-1);
+   Console.WriteLine(result); 
+}
