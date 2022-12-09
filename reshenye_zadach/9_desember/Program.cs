9afa6f4525253f56401e8c05f10e3823f1f13d38
Ekaterina6867 <katya.judo@mail.ru>
2022-12-09 21:56:01 +0300

    создали библиотеку

---
+++ reshenye_zadach/9_desember/Program.cs
@@ -0,0 +1,3 @@
+﻿using static myLybrary;
+ 
+ var arr1 = CreateArray (100);
---
+++ reshenye_zadach/9_desember/myLybrary.cs
@@ -0,0 +1,19 @@
+public static class myLybrary
+{
+    //1.метод создания массива
+
+    public static int[] CreateArray(int count)
+    {
+        return new int []; // new int [count]
+    }
+
+
+//2. метод который заполняет массив
+
+public static void Fill (int[] array)
+{
+    int size = array.Length;
+}
+}
+
+
