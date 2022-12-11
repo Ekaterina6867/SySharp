061c5b6c6aeaa7e1d965172f1e28d2c3dec15447
Ekaterina6867 <katya.judo@mail.ru>
2022-12-11 14:09:17 +0300

    решили вторую задачу

---
+++ dz/dz4/ex002/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+ Console.WriteLine("введите число");
+ int x = Convert.ToInt32(Console.ReadLine());
+ int sum = 0;
+ int num = 0;
+ while (x>0)
+ {
+    num = x%10;
+    sum = sum+num;
+    x=x/10;
+ }
+Console.WriteLine(sum);
+
