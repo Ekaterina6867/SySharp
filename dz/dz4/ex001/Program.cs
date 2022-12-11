376c1465bd2e89795e5bb533ac9ba599a7eed98e
Ekaterina6867 <katya.judo@mail.ru>
2022-12-11 13:28:02 +0300

    решили 1 задачу

---
+++ dz/dz4/ex001/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+
+Console.WriteLine("введите первое число");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите первое число");
+int y = Convert.ToInt32(Console.ReadLine());
+int K = 0;
+int b = 1;
+while (K!=y)
+{
+K++;
+b*=x;
+}
+Console.WriteLine(b);
