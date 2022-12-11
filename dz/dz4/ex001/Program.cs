e7ddeab865b6cf13761961d4f6cebc4aca214554
Ekaterina6867 <katya.judo@mail.ru>
2022-12-11 13:46:30 +0300

    исправили первую задачку

--- dz/dz4/ex001/Program.cs
+++ dz/dz4/ex001/Program.cs
@@ -1,11 +1,11 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
 
 Console.WriteLine("введите первое число");
-int x = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите первое число");
-int y = Convert.ToInt32(Console.ReadLine());
-int K = 0;
-int b = 1;
+double x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+double y = Convert.ToInt32(Console.ReadLine());
+double K = 0;
+double b = 1;
 while (K!=y)
 {
 K++;
