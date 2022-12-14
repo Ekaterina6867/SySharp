88a22f3e56122c36ecb19d87fab0136a4063bab1
Ekaterina6867 <katya.judo@mail.ru>
2022-12-14 12:18:30 +0300

    решили первую задачу

---
+++ reshenye_zadach/ex001/Program.cs
@@ -0,0 +1,15 @@
+﻿// по двум заданным числам проверить является ли первое квадратом второго
+
+Console.WriteLine("ведите число");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ведите второе число");
+int x = Convert.ToInt32(Console.ReadLine());
+if (y==x*x)
+{
+    Console.WriteLine("true");
+}
+else
+{
+    Console.WriteLine("false");
+}
+
