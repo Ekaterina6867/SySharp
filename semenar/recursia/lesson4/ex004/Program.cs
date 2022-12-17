f95ab5162a223d350cee9a14b0f20bc916dca574
Ekaterina6867 <katya.judo@mail.ru>
2022-12-17 13:44:46 +0300

    задача нахождения факториала

---
+++ semenar/recursia/lesson4/ex004/Program.cs
@@ -0,0 +1,13 @@
+﻿// Рекурсия. Напишем метод, который будет принимать число и возвращать факториал этого числа
+
+double Factorial( int n)
+{
+    if ( n==1) return 1; // !1=1  !0=1
+    else return n*Factorial(n-1);
+}
+for (int i =1; i < 40; i++)
+{
+   Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
+
