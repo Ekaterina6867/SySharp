bccb46c2818a2680c3cb544fa8b067e80e43378d
Ekaterina6867 <katya.judo@mail.ru>
2022-12-17 13:53:58 +0300

    задачка финобаччи

---
+++ semenar/recursia/finobacci/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+ double Finobacci (int n)
+ {
+    if (n ==1 || n==2) return 1;
+    else return Finobacci(n-1)+Finobacci(n-2);
+ }
+for (int i = 1; i < 10; i++)
+{
+    Console.WriteLine($"f({i})= {Finobacci(i)}"); //Console.WriteLine(Finobacci(i))
+}
