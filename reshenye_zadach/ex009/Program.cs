0c73a1ca94322e9478abf2c7c35b0b263b3cd9f8
Ekaterina6867 <katya.judo@mail.ru>
2022-12-17 23:53:34 +0300

    Показать последнюю цифру трёхзначного числа

---
+++ reshenye_zadach/ex009/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine("введите трехзначное число");
+double x = Convert.ToInt32(Console.ReadLine());
+double y = x%10;
+Console.WriteLine(y);
+
