00e3e169f3139b84a4d46b38d6943640fe88fd02
Ekaterina6867 <katya.judo@mail.ru>
2022-12-17 13:16:41 +0300

    написали, как раскрасить массив

---
+++ semenar/recursia/lesson4/raskraska/Program.cs
@@ -0,0 +1,35 @@
+﻿// раскрасить массив
+// 1. Определим точку внутри массива (замкнутого контура)
+// 2.  Определимправила обхода ( например: вверх, в лево, вниз, в право)
+int [,]pic = new int [,]; // предусмотрено, что этот массив уже задан ранее, поэтому я не могу запустить программу.
+
+void PrintImage(int[,] image)
+{
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+           if(image[i,j] == 0) Console.Write($" "); // если текущий элемент равен 0, то пишем пробел 
+           else Console.WriteLine($"+"); //если не выполняется предыдущее условие, то ставим (+)
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillImage (int row, int col) // метод, чтобы закрасить массив, в скобках  позиция пикселя.
+{
+    if(pic[row,col] == 0 ) // если пиксель текущей строки не закрашен, т.е 0. (закрашен==1)
+    {
+        pic[row,col]=1; // т.е. будем закрашивать
+        FillImage(row-1, col);
+        FillImage(row, col-1);
+        FillImage(row+1, col);
+        FillImage(row, col+1);
+    } 
+
+}
+
+
+PrintImage(pic);
+FillImage(13,13);
+PrintImage(pic);
