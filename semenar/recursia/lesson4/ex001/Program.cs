0e183d1a473ac87b33e802a0b2cf798570d3e3b7
Ekaterina6867 <katya.judo@mail.ru>
2022-12-17 10:30:06 +0300

    написали две задачи из лекции

---
+++ semenar/recursia/lesson4/ex001/Program.cs
@@ -0,0 +1,14 @@
+﻿// Двумерные массивы
+
+string[,] table = new string[2,5];
+table [1,2] = "слово"; // в строке 1 столбце два будет "слово"
+// table [0,0]  table [0,1]  table [0,2] .....table[0,4]
+// table [1,0]  table [1,1]   table [1,2].....table[0,4]
+for (int i = 0; i < 2; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        Console.Write($"-{table[i,j]}-");
+    }
+    Console.WriteLine();// переход на новую строчку
+}
---
+++ semenar/recursia/lesson4/ex002/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+ void PrintArray(int[,] matrix) //метод, который будет печатать массив
+ {
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+          Console.Write($"{matrix[i,j]}");
+        }
+    }
+    Console.WriteLine();
+ }
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);// заполнение массива рандомными числами от [1 до 10)
+        }
+    }
+}
+
+int [,] array = new int [3,4];
+PrintArray(array);
+FillArray(array);
+Console.WriteLine();
+PrintArray(array);
