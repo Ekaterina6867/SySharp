289cb79f762bbf99738d5500c62e26146994b846
Ekaterina6867 <katya.judo@mail.ru>
2022-12-17 23:13:33 +0300

    Выяснить является ли число чётным

---
+++ reshenye_zadach/ex006/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить является ли число чётным
+Console.WriteLine("введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x%2==0) 
+{
+    Console.WriteLine("число четное");
+}
+else
+{
+    Console.WriteLine("число нечетное");
+}
