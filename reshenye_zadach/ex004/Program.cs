6094762822076e03b1a90de89cea183a8e230e77
Ekaterina6867 <katya.judo@mail.ru>
2022-12-17 22:52:58 +0300

    решили первую задачу

---
+++ reshenye_zadach/ex004/Program.cs
@@ -0,0 +1,19 @@
+﻿//  4. Найти максимальное из трех чисел
+
+Console.WriteLine("введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b>a) 
+{
+   max=b; 
+}
+if (c>a) 
+{
+   max=c; 
+} 
+Console.WriteLine(max);
+
