cc52a0c60b46a7197c7e7b62b63a7b5512c84587
Ekaterina6867 <katya.judo@mail.ru>
2022-12-17 23:47:36 +0300

    четные числа от 1 до N

---
+++ reshenye_zadach/ex008/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать четные числа от 1 до N
+
+Console.WriteLine("введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i <= n; i++)
+{
+    if(i%2==0)
+    {
+        Console.WriteLine(i);
+        
+    }
+}
