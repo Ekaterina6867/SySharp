c43debc2abb02ad50ae9c1dbd6928174f4381c5b
Ekaterina6867 <katya.judo@mail.ru>
2022-12-17 20:05:04 +0300

    решили вторую задачку

---
+++ reshenye_zadach/ex002/Program.cs
@@ -0,0 +1,8 @@
+﻿//Даны два числа. Показать большее и меньшее число
+
+Console.WriteLine("введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int y = Convert.ToInt32(Console.ReadLine());
+if(x>y) Console.WriteLine($"{x}-максимальное, а {y}-минимальное");
+else Console.WriteLine($"{y}-максимальное, а {x}-минимальное");
