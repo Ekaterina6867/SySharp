7844afbcd00cdb5ff090a45cacac6fa49bccc10a
Ekaterina6867 <katya.judo@mail.ru>
2022-12-18 13:07:15 +0300

    Дано число из отрезка [10, 99]. Показать наибольшую цифру числа

---
+++ reshenye_zadach/ex011/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+Console.WriteLine("введите число от 10 до 99 включительно");
+int x = Convert.ToInt32(Console.ReadLine());
+while( x<100 && x>9)
+{
+        int y = x%10;
+    int c = x/10;
+    if(y>c)
+    {
+        Console.WriteLine(y);
+        break;
+
+    }
+    else
+    {
+        Console.WriteLine(c);
+        break;
+    }
+}
+    
