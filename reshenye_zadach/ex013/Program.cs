5e5c37879711334e70db5a6a4a5bda7b4e945981
Ekaterina6867 <katya.judo@mail.ru>
2022-12-18 13:25:29 +0300

    // Выяснить, кратно ли число заданному, если нет, вывести остаток.

---
+++ reshenye_zadach/ex013/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("введите первое число");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int y = Convert.ToInt32(Console.ReadLine());
+if(x%y==0)
+{
+    Console.WriteLine($" {x} кратно {y}");
+}
+else
+{
+
+    Console.WriteLine($"{x%y}");
+}
