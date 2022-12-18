c8d21ba703dd2ac7cded00e2c2cb071ccb0a4e97
Ekaterina6867 <katya.judo@mail.ru>
2022-12-18 12:44:09 +0300

    найти значение функции y=f(a)

---
+++ reshenye_zadach/ex005/Program.cs
@@ -0,0 +1,7 @@
+﻿// Написать программу вычисления значения функции y = f(a)   y=sin(a)
+
+Console.WriteLine("введите число");
+double x = Convert.ToInt32(Console.ReadLine());
+double y = Math.Sin(x);
+Console.WriteLine(y);
+
