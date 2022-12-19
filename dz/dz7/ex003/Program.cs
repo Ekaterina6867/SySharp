4eab261413246a0e5f76be596f4f841d52492cf0
Ekaterina6867 <katya.judo@mail.ru>
2022-12-19 23:47:14 +0300

    Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.

---
+++ dz/dz7/ex003/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void FillArray(int[,]array)//заполняем масив рандомными числами
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j <array.GetLength(1); j++)
+        {
+           array[i,j] = new Random().Next(1,9);
+        }
+    }
+
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i,j] + " "}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SrArf (int[,] arr) // метод нахождения среднего арифметического
+{
+    
+     for (int j = 0; j <arr.GetLength(1); j++)
+     {
+        double summ=0;
+    
+         for (int i = 0; i < arr.GetLength(1); i++)
+         {    
+         summ=summ+arr[i,j];
+              
+
+         }
+         Console.WriteLine($"среднее арифметическое {j} строки { summ / arr.GetLength(0)} ");
+     }
+    
+}
+
+
+
+int [,]array = new int  [2,4];
+FillArray(array);
+PrintArray(array);
+SrArf(array);
