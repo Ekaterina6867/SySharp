fac059a7cd61de9c3a6da63d554292bffb1506a1
Ekaterina6867 <katya.judo@mail.ru>
2022-12-19 16:56:26 +0300

    исправили ошибку

--- dz/dz6/ex02/Program.cs
+++ dz/dz6/ex02/Program.cs
@@ -1,12 +1,12 @@
 ﻿//  Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 Console.WriteLine("введите число b1");
-double b1 = int.Parse(Console.ReadLine());
+double b1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите число k1");
-double k1 = int.Parse(Console.ReadLine());
+double k1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите число k2");
-double k2 = int.Parse(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите число b2");
-double b2 = int.Parse(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
 double x = (b2-b1)/(k1-k2);
 double y = k2 * x + b2;
 
