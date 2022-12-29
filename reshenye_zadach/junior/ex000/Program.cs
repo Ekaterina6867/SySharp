c159725a85fea35daee21818d00a8576d2832c6a
Ekaterina6867 <katya.judo@mail.ru>
2022-12-29 22:51:38 +0300

    Дано число. Проверить кратно ли оно 7 и 23

---
+++ reshenye_zadach/junior/ex000/Program.cs
@@ -0,0 +1,17 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+Console.WriteLine("введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+void Number (int x)
+{
+    if ( x%7==0 && x%23==0) 
+      {
+      Console.WriteLine("число кратно 7 и 23");
+      }
+      else 
+       {
+        Console.WriteLine("число не кратно 7 и 23");
+       }
+}
+
+Number(x);
