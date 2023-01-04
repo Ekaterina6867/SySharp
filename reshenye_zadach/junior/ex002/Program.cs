3b00ac6425ab761a7ad453d7d39e23564fdc608c
Ekaterina6867 <katya.judo@mail.ru>
2023-01-04 22:54:53 +0300

    Программа проверяет пятизначное число на палиндромом.

---
+++ reshenye_zadach/junior/ex002/Program.cs
@@ -0,0 +1,17 @@
+﻿//21. Программа проверяет пятизначное число на палиндромом.
+void Palindrome(int a)
+{
+    int b = a % 10;
+    int c = a / 10000;
+    int d = (a / 10) % 10;
+    int e = (a / 1000) % 10;
+    if(a>9999 && a<100000)
+    {
+    if (b == c && d == e) Console.WriteLine($"число {a} - палиндром");
+    else Console.WriteLine($"число {a} - не палиндром");
+    }
+    else Console.WriteLine("неверное число");
+}
+Console.WriteLine("введите пятизначное число");
+int x = Convert.ToInt32(Console.ReadLine());
+Palindrome(x);
