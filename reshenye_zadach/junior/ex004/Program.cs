2d561b83b9d60277cd5a868d143be3cfe4de499f
Ekaterina6867 <katya.judo@mail.ru>
2023-01-08 14:22:07 +0300

    Показать таблицу квадратов чисел от 1 до N

---
+++ reshenye_zadach/junior/ex004/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Показать таблицу квадратов чисел от 1 до N 
+
+
+    Console.WriteLine("введите число");
+    int x = Convert.ToInt32(Console.ReadLine());
+    int result = 0;
+        while(x>0 )
+    {
+        result = x*x;
+        x=x-1;
+        Console.WriteLine(result);
+    }
+    
+
